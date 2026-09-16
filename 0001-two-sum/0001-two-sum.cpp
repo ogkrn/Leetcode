61eae6b4dc3522b88cb2f09bc9a02825431f2baf
Karan Joshi <krnjoshi1542@gmail.com>
2026-09-16 11:28:35 +0530

    Time: 4 ms (53.1%), Space: 14.8 MB (44.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // input 
-        unordered_map<int, int>hashTable;
+      //input
+        unordered_map<int,int>hashtable;
+      //what dsa to use - Hashtable
+      for(int i=0;i<nums.size();i++ ){  
+      int complement = target - nums[i];
 
-        // What dsa to use : Hashtable
-        for(int i = 0; i<nums.size(); i++){
-            int complement = target - nums[i];
-
-            if (hashTable.count(complement)){ //count() checks if the key is in the hashmap.
-                return{hashTable[complement],i};
-            }
-        hashTable[nums[i]] = i;
-        }
-
-        // What to do with data:
-             return{};
-
-        //output
+      if(hashtable.count(complement)){
+        return{hashtable[complement],i};
+      }
+        hashtable[nums[i]] = i;
     }
-};
+    return {};
+}};
